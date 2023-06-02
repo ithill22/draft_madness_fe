cc666aed5147c1a370a142437c0ac1e079e35dad
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-06-02 16:00:33 -0600

    feat: Add create action functionality.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,36 @@ class UsersController < ApplicationController
   def new; end
 
   def create
-    auth_hash = request.env['omniauth.auth']
-    google_id = auth_hash['uid']
-    email = auth_hash['info']['email']
-    name = auth_hash['info']['name']
-    token = auth_hash['credentials']['token']
+    DraftMadnessService.new.register_user(user_params)
+    redirect_to '/sessions#create'
   end
 
-  def dashboard; end
+  def show; end
+
+  private
+
+  def user_params
+    {
+      name: name,
+      email: email,
+      google_id: google_id,
+      auth_token: token
+    }
+  end
+
+  def google_id
+    request.env['omniauth.auth']['uid']
+  end
+
+  def email
+    request.env['omniauth.auth']['info']['email']
+  end
+
+  def name
+    request.env['omniauth.auth']['info']['name']
+  end
+
+  def token
+    request.env['omniauth.auth']['credentials']['token']
+  end
 end
