4983f4d50d264d2c81ad4e03ea8d32a67a4cab61
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-06-02 15:54:39 -0600

    test: Add vcr configuration.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -65,3 +65,10 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  config.hook_into :webmock
+  config.default_cassette_options = { re_record_interval: 7.days }
+  config.configure_rspec_metadata!
+end
