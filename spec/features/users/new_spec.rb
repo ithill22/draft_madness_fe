81710e6f199cb36f6db5eb5be57889868afb1f50
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-06-03 10:35:24 -0600

    test: Add test for oauth.

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -2,10 +2,27 @@ require 'rails_helper'
 
 RSpec.describe 'new user page' do
   describe 'As a visitor, when I visit the new user page' do
-    it 'has a form to create a new user' do
-      visit new_user_path
+    before :each do
+      OmniAuth.config.test_mode = true
+      OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
+        provider: 'google_oauth2',
+        uid: '123545',
+        info: {
+          name: 'Alec',
+          email: 'amk@gmail.com'
+        },
+        credentials: {
+          token: '1244hjgjh54jg333h4kj5l5k6'
+        }
+      })
 
-      
+      visit new_user_path
+    end
+
+    it 'has a link to signup via google oauth', :vcr do
+      click_link 'Sign Up With Google'
+
+      expect(current_path).to eq(users_dashboard_path)
     end
   end
 end
