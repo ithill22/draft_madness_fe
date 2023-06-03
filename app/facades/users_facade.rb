6e289352133356a3006c7c38ab1f249527a8e1fd
Katie Lonsdale <katie952@gmail.com>
2023-06-03 17:57:19 -0600

    feat: add additional methods

---
+++ app/facades/users_facade.rb
@@ -0,0 +1,29 @@
+class UsersFacade
+  def initialize(session_id = nil)
+    @session_id = session_id
+  end
+
+  def user
+    User.new(user_details[:data])
+  end
+
+  def all_users
+    users_details[:data].map do |user|
+      User.new(user[:attributes])
+    end
+  end
+
+  private
+
+  def service
+    @_service ||= DraftMadnessService.new
+  end
+
+  def user_details
+    @_user_details ||= service.get_one_user(@session_id)
+  end
+
+  def users_details
+    @_users_details ||= service.get_all_users
+  end
+end
