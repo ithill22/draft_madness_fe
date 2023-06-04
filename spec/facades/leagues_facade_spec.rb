87b7b5f2866539dbec680b112cf5f470e3c0066a
Katie Lonsdale <katie952@gmail.com>
2023-06-04 09:40:29 -0600

    test: add test for league

--- spec/facades/leagues_facade_spec.rb
+++ spec/facades/leagues_facade_spec.rb
@@ -1,17 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe LeaguesFacade do
-  describe 'new_league' do
-    it 'returns a league poro', :vcr do
-      league_details = {
+  before(:each) do
+    league_details = {
         :name=>"League Name", 
         :draft_date=>"June 25, 2023", 
         :draft_time=>"8:30 pm", 
         :manager_id=>2}
 
-      lf = LeaguesFacade.new.new_league(league_details)
-      expect(lf).to be_a(League)
-      expect(lf.name).to eq("League Name")
+    @lf = LeaguesFacade.new.new_league(league_details)
+  end
+  describe 'new_league' do
+    it 'returns a league poro', :vcr do
+      expect(@lf).to be_a(League)
+      expect(@lf.name).to eq("League Name")
+    end
+  end
+  describe 'league' do
+    it 'returns league details', :vcr do
+      league = LeaguesFacade.new.league(@lf.id)
+
+      expect(league).to be_a(League)
+      expect(league.name).to eq("League Name")
     end
   end
 end
