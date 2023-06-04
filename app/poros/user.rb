3f611c47490098c8d1a439d6997fa14142895d1c
Katie Lonsdale <katie952@gmail.com>
2023-06-03 19:15:35 -0600

    refactor: remove google id and auth token

--- app/poros/user.rb
+++ app/poros/user.rb
@@ -1,10 +1,8 @@
 class User
-  attr_reader :id, :name, :email, :google_id, :auth_token
+  attr_reader :id, :name, :email
   def initialize(data)
     @id = data[:id]
     @name = data[:name]
     @email = data[:email]
-    @google_id = data[:google_id]
-    @auth_token = data[:auth_token]
   end
 end
