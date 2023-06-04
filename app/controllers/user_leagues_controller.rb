99a96fd8df7cf202f32dfa571ffd9a93641e563b
Katie Lonsdale <katie952@gmail.com>
2023-06-04 15:39:06 -0600

    feat: add create action

---
+++ app/controllers/user_leagues_controller.rb
@@ -0,0 +1,14 @@
+class UserLeaguesController < ApplicationController
+  def new 
+  end
+
+  def create
+    if params[:participants]
+      params[:participants].each do |participant|
+        user_id = participant.to_i
+        UserLeaguesFacade.new.create_user_league({league_id: params[:league].to_i, user_id: participant.to_i})
+      end
+    end
+    redirect_to league_path(params[:league])
+  end
+end
