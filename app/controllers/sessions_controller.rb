74fbc2318bb2ff971ec381e556a94a965fae555b
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-06-03 19:54:10 -0600

    feat: Modify session user_id.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    session[:user_id] = params[:id]
+    session[:user_id] = params[:data][:attributes][:auth_token]
 
     redirect_to users_dashboard_path
   end
