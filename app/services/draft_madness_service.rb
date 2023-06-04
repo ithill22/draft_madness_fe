367ce41f9b04ccd7224ae6488d6e85ff1bc81018
Katie Lonsdale <katie952@gmail.com>
2023-06-04 14:59:14 -0600

    feat: add new_user_league method

--- app/services/draft_madness_service.rb
+++ app/services/draft_madness_service.rb
@@ -19,6 +19,10 @@ class DraftMadnessService
     get_url("/api/v0/leagues/#{id}")
   end
 
+  def new_user_league(ul_details)
+    ul_post_url('/api/v0/user_leagues', ul_details)
+  end
+
   private
 
   def post_url(url, user_details)
@@ -28,6 +32,11 @@ class DraftMadnessService
 
   def league_post_url(url, league_details)
     response = conn.post(url, league: league_details, header: { 'CONTENT_TYPE' => 'application/json' })
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def ul_post_url(url, ul_details)
+    response = conn.post(url, user_league: {league_id: ul_details[:league_id], user_id: ul_details[:user_id]}, header: { 'CONTENT_TYPE' => 'application/json' })
     JSON.parse(response.body, symbolize_names: true)
   end
 
