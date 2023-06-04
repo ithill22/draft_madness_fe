6e61a88601bda8a8fe318d0e9708205a1f570aa7
Katie Lonsdale <katie952@gmail.com>
2023-06-04 09:40:37 -0600

    feat: add league method

--- app/facades/leagues_facade.rb
+++ app/facades/leagues_facade.rb
@@ -1,6 +1,10 @@
 class LeaguesFacade
   def new_league(league_params)
-    League.new(league_details(league_params)[:data])
+    League.new(new_league_details(league_params)[:data])
+  end
+
+  def league(id)
+    League.new(league_details(id)[:data])
   end
 
   private
@@ -9,7 +13,11 @@ class LeaguesFacade
     @_service ||= DraftMadnessService.new
   end
 
-  def league_details(league_params)
-    @_league_details ||= service.create_league(league_params)
+  def new_league_details(league_params)
+    @_new_league_details ||= service.create_league(league_params)
+  end
+
+  def league_details(id)
+    @_league_details ||= service.get_one_league(id)
   end
 end
