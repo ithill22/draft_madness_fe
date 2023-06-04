0d16261bf027d2320a499ce403555b3d27028545
Katie Lonsdale <katie952@gmail.com>
2023-06-03 19:46:52 -0600

    test: add new league test

---
+++ spec/facades/leagues_facade_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe LeaguesFacade do
+  describe 'new_league' do
+    it 'returns a league poro', :vcr do
+      league_details = {
+        :name=>"League Name", 
+        :draft_date=>"June 25, 2023", 
+        :draft_time=>"8:30 pm", 
+        :manager_id=>2}
+
+      lf = LeaguesFacade.new.new_league(league_details)
+      expect(lf).to be_a(League)
+    end
+  end
+end
