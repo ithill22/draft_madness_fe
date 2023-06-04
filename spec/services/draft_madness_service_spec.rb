aa521b0b853563df21e06853d58368b1181e0e6c
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-06-03 19:46:06 -0600

    test: Modify session_ids in tests.

--- spec/services/draft_madness_service_spec.rb
+++ spec/services/draft_madness_service_spec.rb
@@ -33,5 +33,39 @@ RSpec.describe DraftMadnessService do
         expect(user_data[:auth_token]).to be_a(String)
       end
     end
+
+    context 'get_one_user' do
+      it 'returns user details', :vcr do
+        user_details =
+          {
+            name: 'Joe',
+            email: 'joe@gmail.com',
+            google_id: '412gfhgf235',
+            auth_token: 'hjgu.734764g34734h347hdf7d7d6'
+        }
+        DraftMadnessService.new.register_user(user_details)
+        session_id = 'hjgu.734764g34734h347hdf7d7d6'
+        results = DraftMadnessService.new.get_one_user(session_id)
+
+        expect(results).to be_a(Hash)
+
+        expect(results).to have_key(:data)
+        expect(results[:data]).to have_key(:attributes)
+        expect(results[:data]).to have_key(:id)
+        expect(results[:data]).to have_key(:type)
+
+        user_data = results[:data][:attributes]
+
+        expect(user_data).to have_key(:name)
+        expect(user_data).to have_key(:email)
+        expect(user_data).to have_key(:google_id)
+        expect(user_data).to have_key(:auth_token)
+
+        expect(user_data[:name]).to be_a(String)
+        expect(user_data[:email]).to be_a(String)
+        expect(user_data[:google_id]).to be_a(String)
+        expect(user_data[:auth_token]).to be_a(String)
+      end
+    end
   end
 end
