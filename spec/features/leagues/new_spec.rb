5685438d0cc36ba164a296e31a1d137d09fc0695
Katie Lonsdale <katie952@gmail.com>
2023-06-05 17:41:00 -0600

    test: add test for flash message if wrong number of players is selected

--- spec/features/leagues/new_spec.rb
+++ spec/features/leagues/new_spec.rb
@@ -46,16 +46,29 @@ RSpec.describe 'new league page' do
         expect(page).to have_content("Please fill in all fields.")
       end
     end
-    xit 'returns an error if more than 8 participants are selected' do
+    it 'returns an error if more or less than 8 participants are selected', :vcr do
       too_many_participants
       users = UsersFacade.new.all_users
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return('hjgu.734764g34734h347hdf7d7d6444')
+      
+      fill_in :name, with: 'League Name'
+      select "2023", from: "[day(1i)]"
+      select "June", from: "[day(2i)]"
+      select "25", from: "[day(3i)]"
+      select "08 PM", from: "[time(4i)]"
+      select "30", from: "[time(5i)]"
       users.each do |user|
         within("#user-#{user.id}") do
           expect(page).to have_field(user.id.to_s, type: 'checkbox')
           check(user.id)
         end
-      end      
+      end
+      click_button "Submit"
+
+      expect(current_path).to eq(new_league_path)
+      within("#flash") do
+        expect(page).to have_content("Please choose 8 players.")
+      end
     end
   end
 end
