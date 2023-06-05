8f50e4ed3620f569ab7d157efc2457aaabb1854f
Katie Lonsdale <katie952@gmail.com>
2023-06-05 17:26:29 -0600

    feat: add sad path for new league

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -1,19 +1,26 @@
 class LeaguesController < ApplicationController
+  before_action :leagues_facade, only: [:create, :show]
+
   def new
     @users = UsersFacade.new.all_users
   end
 
   def create
-    @user = UsersFacade.new(current_user).user
-    league = LeaguesFacade.new.new_league(league_params)
-    redirect_to controller: 'user_leagues',
-                  action: 'create',
-                  participants: params[:participants],
-                  league: league.id
+    if !@lf.empty_params?(params)
+      @user = UsersFacade.new(current_user).user
+      league = @lf.new_league(league_params)
+      redirect_to controller: 'user_leagues',
+                    action: 'create',
+                    participants: params[:participants],
+                    league: league.id
+    else
+      flash[:error] = "Please fill in all fields."
+      redirect_to new_league_path
+    end
   end
 
   def show
-    @league = LeaguesFacade.new.league(params[:id])
+    @league = @lf.league(params[:id])
   end
 
   private
@@ -33,5 +40,9 @@ class LeaguesController < ApplicationController
 
   def format_time
     Time.parse("#{params['time(4i)']}:#{params['time(5i)']}").strftime("%-l:%M %P")
+  end
+
+  def leagues_facade
+    @lf = LeaguesFacade.new
   end
 end
