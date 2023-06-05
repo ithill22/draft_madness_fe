7a81650e337f0bc616d7dbd2ea160ed2bcfeda2e
Katie Lonsdale <katie952@gmail.com>
2023-06-05 17:26:16 -0600

    test: add user data

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -85,3 +85,47 @@ def user_data
     DraftMadnessService.new.register_user(user)
   end
 end
+
+def too_many_participants
+  @user_1 = {:name=>"Alec", :email=>"amk@gmail.com", :google_id=>"123345", :auth_token=>"hjgu.734764g34734h347hdf7d7d6444"}
+  @user_2 = {:name=>"Steve", :email=>"kcl@gmail.com", :google_id=>"123645", :auth_token=>"sdrf.1244h2342454kj5l5k6"}
+  @user_3 = {:name=>"George", :email=>"mjm@gmail.com", :google_id=>"123745", :auth_token=>"drsr.1244hjgjh234959525l5k6"}
+  @user_4 = {:name=>"Ted", :email=>"tct@gmail.com", :google_id=>"123945", :auth_token=>"plrd.1244hjgjh234959525l5k6"}
+  @user_5 = {:name=>"Katie", :email=>"123@gmail.com", :google_id=>"123045", :auth_token=>"hjgp.734764g34734h347hdf7d7d6444"}
+  @user_6 = {:name=>"Sally", :email=>"4576@gmail.com", :google_id=>"123605", :auth_token=>"serf.1244h2342454kj5l5k6"}
+  @user_7 = {:name=>"Georgia", :email=>"kdr@gmail.com", :google_id=>"123740", :auth_token=>"drsr.124hhjgjh234959525l5k6"}
+  @user_8 = {:name=>"Tina", :email=>"ppp@gmail.com", :google_id=>"023945", :auth_token=>"plrd.1244hjgjh234959525p5k6"}
+  @user_9 = {:name=>"Karen", :email=>"2many@gmail.com", :google_id=>"223945", :auth_token=>"nono.1244hjgjh234959525p5k6"}
+end
+
+def params
+  @complete_params = {"authenticity_token"=>"MGTxscYtvcBohWxdibkiMyhAQSKbfF_8yJEqvjtTV5jKii-M-Z1PLTbbCuN4mpZplPM1WBT4YD_aicnZHONIxQ", 
+    "name"=>"Katie", 
+    "participants"=>["1"], 
+    "day(1i)"=>"2023", 
+    "day(2i)"=>"6", 
+    "day(3i)"=>"5", 
+    "time(1i)"=>"2023", 
+    "time(2i)"=>"6", 
+    "time(3i)"=>"5", 
+    "time(4i)"=>"21", 
+    "time(5i)"=>"04", 
+    "commit"=>"Submit", 
+    "controller"=>"leagues", 
+    "action"=>"create"}
+
+  @incomplete_params = {"authenticity_token"=>"MGTxscYtvcBohWxdibkiMyhAQSKbfF_8yJEqvjtTV5jKii-M-Z1PLTbbCuN4mpZplPM1WBT4YD_aicnZHONIxQ", 
+    "name"=>"", 
+    "participants"=>["1"], 
+    "day(1i)"=>"2023", 
+    "day(2i)"=>"6", 
+    "day(3i)"=>"5", 
+    "time(1i)"=>"2023", 
+    "time(2i)"=>"6", 
+    "time(3i)"=>"5", 
+    "time(4i)"=>"21", 
+    "time(5i)"=>"04", 
+    "commit"=>"Submit", 
+    "controller"=>"leagues", 
+    "action"=>"create"}
+end
