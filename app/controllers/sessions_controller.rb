4d2500ba6898a617ff979a5a03504b0bdf723edd
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-06-05 19:23:31 -0600

    fix: Change session id to user id.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    session[:user_id] = params[:data][:attributes][:auth_token]
+    session[:user_id] = params[:data][:id]
 
     redirect_to users_dashboard_path
   end
