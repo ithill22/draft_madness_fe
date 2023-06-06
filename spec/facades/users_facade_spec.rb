a0fbcc0a9fa3b06bb20a41d77c8712e638dc3985
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-06-05 19:16:42 -0600

    test: Add tests for user_name and all_leagues_for_user methods.

--- spec/facades/users_facade_spec.rb
+++ spec/facades/users_facade_spec.rb
@@ -1,40 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe UsersFacade do
-  describe 'user' do
-    it 'returns a user object', :vcr do
-      user_details =
+  describe 'Instance methods' do
+    describe 'user' do
+      it 'returns a user object', :vcr do
+        user_details =
           {
             name: 'Joe',
             email: 'jj@gmail.com',
             google_id: '412gfhgf235',
-            auth_token: 'hjgu.734764g34734h347hdf7d7d6g5g5g'
-        }
-      DraftMadnessService.new.register_user(user_details)
-      session_id = 'hjgu.734764g34734h347hdf7d7d6g5g5g'
-      facade = UsersFacade.new(session_id)
+            auth_token: 'hjgu734764g34734h347hdf7d7d6g5g5g'
+          }
+        dms = DraftMadnessService.new.register_user(user_details)
+        session_id = dms[:data][:id].to_s
+        facade = UsersFacade.new(session_id)
 
-      expect(facade.user).to be_a(User)
-      expect(facade.user_name).to be_a(String)
-      expect(facade.user.email).to be_a(String)
+        expect(facade.user).to be_a(User)
+        expect(facade.user_name).to be_a(String)
+        expect(facade.user.email).to be_a(String)
+      end
     end
-  end
 
-  describe 'user_name' do
-    it 'returns a user object', :vcr do
-      user_details =
-          {
-            name: 'Joe',
-            email: 'jj@gmail.com',
-            google_id: '412gfhgf235',
-            auth_token: 'hjgu.734764g34734h347hdf7d7d6g5g5g'
+    describe 'user_name' do
+      it 'returns a users name', :vcr do
+        user_details =
+        {
+          name: 'Joe',
+          email: 'jj@gmail.com',
+          google_id: '412gfhgf235',
+          auth_token: 'hjgu734764g34734h347hdf7d7d6g5g5g'
         }
-      DraftMadnessService.new.register_user(user_details)
-      session_id = 'hjgu.734764g34734h347hdf7d7d6g5g5g'
-      facade = UsersFacade.new(session_id)
+        dms = DraftMadnessService.new.register_user(user_details)
+        session_id = dms[:data][:id].to_s
+        facade = UsersFacade.new(session_id)
 
+        expect(facade.user_name).to eq('Joe')
+      end
+    end
 
-      expect(facade.user_name).to eq('Joe')
+    describe 'all_leagues_for_user' do
+      it 'returns an array of leagues', :vcr do
+        session_id = '2'
+        facade = UsersFacade.new(session_id)
+
+        expect(facade.all_leagues_for_user).to be_an(Array)
+        expect(facade.all_leagues_for_user.count).to eq(2)
+        expect(facade.all_leagues_for_user.first).to be_a(League)
+        expect(facade.all_leagues_for_user.first.name).to be_a(String)
+      end
     end
   end
 end
