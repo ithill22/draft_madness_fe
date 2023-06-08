4633e452116bc7f674277f98055d7c5b18797f07
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-06-07 19:20:38 -0600

    feat: Add destroy action.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,9 @@ class SessionsController < ApplicationController
     session[:user_id] = params[:data][:id]
     redirect_to users_dashboard_path
   end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
 end
