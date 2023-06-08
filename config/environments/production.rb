8b2a45c4658a059fe6539fa4af57e30b5bb802aa
Katie Lonsdale <katie952@gmail.com>
2023-06-08 16:49:36 -0600

    configure action mailer for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  ruby config.action_mailer.delivery_method = :smtp 
+  config.action_mailer.perform_deliveries = true 
+  config.action_mailer.smtp_settings = { domain: 'draft-madness.herokuapp.com/', address: "smtp.sendgrid.net", port: 587, authentication: :plain, user_name: 'apikey', password: ENV['SENDGRID_API_KEY'] } 
 end
