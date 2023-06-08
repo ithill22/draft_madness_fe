348e55f6914f366cd8183c0040f14702815d402d
Reid Miller <reid.s.miller@gmail.com>
2023-06-08 15:09:58 -0600

    poro: Add team to app poros

--- app/poros/team.rb
+++ app/poros/team.rb
@@ -1,7 +1,11 @@
 class Team
-  attr_reader :id, :type, :name, :seed, :games_won, :region
+  attr_reader :id, :name, :seed, :games_won, :region
 
-  def initialize
-    
+  def initialize(data)
+    @id = data[:id]
+    @name = data[:attributes][:name]
+    @seed = data[:attributes][:seed]
+    @games_won = data[:attributes][:games_won]
+    @region = data[:attributes][:region]
   end
 end
